8be279f40d3083e4e9c17c6beabca31e656cc9a1
Hugo Frias Martinez <a3521110393@alumno.uttehuacan.edu.mx>
2022-10-10 19:29:41 -0500

    :sparkles: (feat): day one python

---
+++ day_01/helloworld.py
@@ -0,0 +1,42 @@
+# this is a slingle line coommet
+# Exercise: Level 1
+# Check the python version you are using
+
+print('pythom3 --version')
+
+# Open the python interactive shell and do the following operations. The operands are 3 and 4.
+
+print(2 +  3)             # addition(+)
+print(3 -  1)             # subtraction(-)
+print(2 *  3)             # multiplication(*)
+print(3 /  2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 %  2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# Write strings on the python interactive shell.
+print('Hugo Frias')
+print('Erwin Alan')
+print('Mexico')
+print('I am enjoying 30 days of python')
+
+# Check the data types of the following data
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Hugo Frias', 'Pythom','Mexico']))
+print(type('Hugo Frias'))
+print(type('Erwin Alan'))
+print(type('Mexico'))
+
+
+# Exercise: Level 2
+print(type(10))                     # Int
+print(type(3.14))                   # Float
+print(type(1 + 3j))                 # Complex number
+print(type('Asabeneh'))             # String
+print(type([1, 2, 3]))              # List
+print(type({'name':'Asabeneh'}))    # Dictionary
+print(type({9.8, 3.14, 2.7}))       # Set
+print(type((9.8, 3.14, 2.7)))       # Tuple
