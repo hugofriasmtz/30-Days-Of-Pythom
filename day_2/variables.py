36c8621d4e0c929d44b461c9cd9baba5fb17cac1
Hugo Frias Martinez <a3521110393@alumno.uttehuacan.edu.mx>
2022-10-11 20:52:56 -0500

    day two python

---
+++ day_2/variables.py
@@ -0,0 +1,54 @@
+ # Day 2: 30 Days Of Python programming
+
+firstName = 'Hugo Frias'
+lastName = 'Frías Martinez'
+fullName = 'Hugo J. Frias Martinez'
+country = 'México'
+city = 'Cuidad de México'
+age = '19'
+year = '2022'
+is_married = False
+is_true = True
+is_light_on = True
+color, size, language, phone = 'Black', 32, 'Español', 2384082268
+
+print(type(firstName))
+print(type(lastName))
+print(type(fullName))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+len(firstName)
+len(lastName)
+
+num_one, num_two = 5, 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_two * num_one
+division = num_two /num_one
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_devision = num_one // num_two
+
+area_of_circle = 3.1416 *30 ** 2
+circum_of_circle = 3.1416 * 60
+radius = int(input('Radius:'))
+area_of_circle = 3.1416 * radius ** 2
+print(area_of_circle)
+
+first_name = input('What is your Name: ')
+last_name  = input('What is your Last Name: ')
+country    = input('What is your Country: ')
+age        = input('How old are you? ')
+
+print(first_name)
+print(last_name)
+print(country)
+print(age)
+
+help('keyworld')
