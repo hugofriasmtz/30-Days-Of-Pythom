dee3a8bb0309355e2fab15fc611863d44dc0bef7
hugofriasmtz <a3521110393@alumno.uttehuacan.edu.mx>
2025-10-29 11:43:01 -0600

    :sparkles: feat: day six  python challenge

---
+++ day_6/day_6.py
@@ -0,0 +1,92 @@
+# Creating a tuple
+
+empty_tuple = ()
+# or using the tuple constructor
+empty_tuple = tuple()
+
+tpl = ('item1', 'item2','item3')
+print(tpl)
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+print(fruits)
+
+# Touple length
+
+# syntax
+tpl = ('item1', 'item2', 'item3')
+print(len(tpl))
+
+# Accessing Tuple Items
+
+# Positive Indexing Similar to the list data type we use positive or negative indexing to access tuple items.
+tpl = ('item1', 'item2', 'item3')
+first_item = tpl[0]
+second_item = tpl[1]
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+last_index =len(fruits) - 1
+last_fruit = fruits[last_index]
+
+# Negative indexing Negative indexing means beginning from the end, -1 refers to the last item, -2 refers to the second last and the negative of the list/tuple length refers to the first item.
+tpl = ('item1', 'item2', 'item3','item4')
+first_item = tpl[-4]
+second_item = tpl[-3]
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[-4]
+second_fruit = fruits[-3]
+last_fruit = fruits[-1]
+
+# Slicing tuples
+
+# Range of Positive Indexes
+tpl = ('item1', 'item2', 'item3','item4')
+all_items = tpl[0:4]         # all items
+all_items = tpl[0:]         # all items
+middle_two_items = tpl[1:3]  # does not include item at index 3
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[0:4]    # all items
+all_fruits= fruits[0:]      # all items
+orange_mango = fruits[1:3]  # doesn't include item at index 3
+orange_to_the_rest = fruits[1:]
+
+# Range of Negative Indexes
+tpl = ('item1', 'item2', 'item3','item4')
+all_items = tpl[-4:]         # all items
+middle_two_items = tpl[-3:-1]  # does not include item at index 3 (-1)
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[-4:]    # all items
+orange_mango = fruits[-3:-1]  # doesn't include item at index 3
+orange_to_the_rest = fruits[-3:]
+
+# Checking an Item in a Tuple
+
+tpl = ('item1', 'item2', 'item3','item4')
+'item2' in tpl # True
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+print('orange' in fruits) # True
+print('apple' in fruits) # False
+fruits[0] = 'apple' # TypeError: 'tuple' object does not support item assignment
+
+# Joining Tuples
+
+tpl1 = ('item1', 'item2', 'item3')
+tpl2 = ('item4', 'item5','item6')
+tpl3 = tpl1 + tpl2
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+vegetables = ('Tomato', 'Potato', 'Cabbage','Onion', 'Carrot')
+fruits_and_vegetables = fruits + vegetables
+
+# Deleting Tuples
+
+tpl1 = ('item1', 'item2', 'item3')
+del tpl1
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+del fruits
